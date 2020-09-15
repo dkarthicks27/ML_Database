4340805a7975be26c95a38af6310c552e8cc73f3
dkarthicks27 <dkarthicks27@gmail.com>
2020-09-16 00:51:19 +0530

    repeated string.py

---
+++ Algorithms/implementation/repeated string.py
@@ -0,0 +1,13 @@
+s = 'aba'
+n = 10
+import re
+
+length = len(s)
+reps, remainder = divmod(n, length)
+print('reps: ', reps, '\nremainder: ', remainder)
+
+new = s[:remainder]
+print('remaining string: ', new)
+
+total = len(re.findall('a', s)) * reps + len(re.findall('a', new))
+print('total: ', total)
