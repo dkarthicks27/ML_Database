4e0bfa50f5b0dc639cf7914f440f38eab233bef0
dkarthicks27 <dkarthicks27@gmail.com>
2020-09-27 05:46:14 +0530

    watermelon.py

--- Algorithms/searching/searching largest smallest ele.py
+++ Algorithms/searching/searching largest smallest ele.py
@@ -14,19 +14,24 @@ def binarySearch(arr, q):
             start = mid + 1
         elif arr[mid] > q:
             end = mid - 1
-    return ans
+    if ans != -1:
+        return arr[ans]
+    else:
+        return None
 
 
 
-arr = [13, 20, 15, 8, 2, 12]
+arr = [5, 10, 3]
 
 
 for i, val in enumerate(arr):
     print(f'i: {i}, val: {val}')
     binary = binarySearch(arr[i+1:], val)
     print(arr)
-    if binary != -1:
-        print('index is:', binary + i+1, 'val is:', arr[binary+i + 1])
-    else:
-        print('no such index found')
+    print(binary)
+    # print(arr)
+    # if binary != -1:
+    #     print('index is:', binary + i+1, 'val is:', arr[binary + i + 1])
+    # else:
+    #     print('no such index found')
     print('\n')
