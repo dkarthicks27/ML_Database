8217f0d2b393d68818aefcb8385ab204eeeaba18
dkarthicks27 <dkarthicks27@gmail.com>
2021-06-11 14:01:04 +0530

    codeforces daily challenge

--- contest/codeforces_round_725_div3/q4.py
+++ contest/codeforces_round_725_div3/q4.py
@@ -1,12 +1,20 @@
 import numpy as np
+import sys
 
-def solve(x, y, a, b):
-    A = np.array([[]])
-    pass
+def solved(x, y, a, b):
+
+    A = np.array([[a, b], [b, a]])
+    B = np.array([x, y])
+    try:
+        sol = list(np.linalg.solve(A, B))
+        print(sum(sol), '\n')
+    except:
+        print(sys.exc_info(), '\n', f'Error is : X, Y, a, b: {(x, y, a, b)}\n\n')
+        pass
 
 
 if __name__ == '__main__':
     t = int(input())
     for _ in range(t):
         x_, y_, a_, b_ = list(map(int, input().split()))
-        solve(x_, y_, a_, b_)
+        solved(x_, y_, a_, b_)
