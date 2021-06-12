ce2ee1009fcf31aee6f0f89f5664c055f6a86ed4
dkarthicks27 <dkarthicks27@gmail.com>
2021-06-12 22:39:40 +0530

    daily challenge

---
+++ codeforces/buy_a_shovel.py
@@ -0,0 +1,6 @@
+if __name__ == '__main__':
+    k, r = list(map(int, input().split()))
+    for i in range(1, 10):
+        if k * i % 10 == 0 or k * i % 10 == r:
+            print(i)
+            break
