e7846a2de28ed792f1e6554c32da7ae3d7887ff8
dkarthicks27 <dkarthicks27@gmail.com>
2021-06-16 19:17:04 +0530

    codeforces daily challenge

--- codeforces/gennady_and_a_card_game.py
+++ codeforces/gennady_and_a_card_game.py
@@ -1,2 +1,14 @@
+def solve(tab, hand):
+    for i in hand:
+        if i[0] == tab[0] or i[1] == tab[1]:
+            return 'YES'
+
+    return 'NO'
+
+
+
 if __name__ == '__main__':
-    pass
+    card_on_table = input()
+    card_on_hand = input().split()
+    ans = solve(card_on_table, card_on_hand)
+    print(ans)
