cf59dc50d16182915606e5a5ccc76144149128fc
dkarthicks27 <dkarthicks27@gmail.com>
2021-06-17 11:35:42 +0530

    codeforces daily challenge

---
+++ codeforces/Mahmoud and Ehab and the even-odd game.py
@@ -0,0 +1,8 @@
+if __name__ == '__main__':
+    n = int(input())
+    if n == 1:
+        print("Ehab")
+    elif n % 2 == 0 and n >= 2:
+        print('Mahmoud')
+    else:
+        print('Ehab')
---
+++ codeforces/copycopy.py
@@ -0,0 +1,7 @@
+if __name__ == '__main__':
+    t = int(input())
+    for _ in range(t):
+        n = int(input())
+        arr = list(map(int, input().split()))
+        potential = list(set(arr))
+        print(len(potential))
