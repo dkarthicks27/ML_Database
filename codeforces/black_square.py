0d25d5583f49551d9d344e806b105c540fb7ac60
dkarthicks27 <dkarthicks27@gmail.com>
2021-07-09 16:00:21 +0530

    cdc

--- codeforces/black_square.py
+++ codeforces/black_square.py
@@ -1,10 +1,31 @@
-if __name__ == '__main__':
-    calorie_each_pos = input().split()
-    ref = {str(i+1): int(calorie_each_pos[i]) for i in range(len(calorie_each_pos))}
-    s = input()
+# if __name__ == '__main__':
+#     calorie_each_pos = input().split()
+#     ref = {str(i+1): int(calorie_each_pos[i]) for i in range(len(calorie_each_pos))}
+#     s = input()
+#
+#     count = 0
+#     for i in s:
+#         count += ref[i]
+#
+#     print(count)
+from collections import OrderedDict
 
-    count = 0
-    for i in s:
-        count += ref[i]
+k = 'cir'
+l = 'car'
+arr = [set(k), set(l)]
 
-    print(count)
+res = set.intersection(*arr)
+print(res)
+new_arr = [0 for i in range(20)]
+for i in res:
+    new_arr[k.index(i)] = i
+
+new_arr = [i for i in new_arr if i != 0]
+output = ''.join(new_arr)
+print(output)
+while True:
+    if output in k:
+        print(output)
+        break
+    else:
+        output = output[:-1]
