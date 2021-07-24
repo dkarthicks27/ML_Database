7229f49edde506896f2396f30976f1d92bfb5f89
dkarthicks27 <dkarthicks27@gmail.com>
2021-07-25 00:56:59 +0530

    cdc

--- codeforces/last years substring.py
+++ codeforces/last years substring.py
@@ -1,2 +1,21 @@
+import re
+
+
 if __name__ == '__main__':
-    
+    # possible strings are 2020, 2.*020, 20.*20, 202.*0
+    t = int(input())
+    for _ in range(t):
+        n = int(input())
+        string = input()
+        pattern = [r'^2020', r'2020$', r'^2.*020$', r'^20.*20$', r'^202.*0$']
+        q = 0
+        for i in pattern:
+            if re.search(i, string):
+                print('YES')
+                q = 1
+                break
+
+        if q == 0:
+            print('NO')
+
+
