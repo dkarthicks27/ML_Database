1fa8483419accb9b9babfe82b7df492da576ba1c
dkarthicks27 <dkarthicks27@gmail.com>
2021-08-04 22:41:36 +0530

    cdc

--- codeforces/three swimmers.py
+++ codeforces/three swimmers.py
@@ -2,4 +2,7 @@ if __name__ == '__main__':
     t = int(input())
     for _ in range(t):
         p, a, b, c = list(map(int, input().split()))
-        print(min(a, b, c) - p)
+        a = (a - p % a) % a
+        b = (b - p % b) % b
+        c = (c - p % c) % c
+        print(min(a, b, c))
